9d582d8715976fb6a40a2e88b3066e8c8391393c
Marcos Vinícius Montanari <marcos.montanari@ifnmg.edu.br>
2026-03-11 22:10:27 -0300

    Ajusta rotas do frontend para produção

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const responsaveisRoutes = require('./routes/responsaveis');
 const tarefasRoutes = require('./routes/tarefas');
@@ -11,10 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Servir arquivos estáticos do frontend
+app.use('/css', express.static(path.join(__dirname, '../frontend/css')));
+app.use('/js', express.static(path.join(__dirname, '../frontend/js')));
+app.use('/pages', express.static(path.join(__dirname, '../frontend/pages')));
+
+// Página inicial do site
 app.get('/', (req, res) => {
-    res.json({ mensagem: 'API do app-tarefas funcionando!' });
+    res.sendFile(path.join(__dirname, '../frontend/pages/index.html'));
 });
 
+// Rotas da API
 app.use('/responsaveis', responsaveisRoutes);
 app.use('/tarefas', tarefasRoutes);
 app.use('/execucoes', execucoesRoutes);
